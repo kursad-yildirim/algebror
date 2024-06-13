ea007a9a43e98e7a25872fd26ba48404441a22d0
kursad-yildirim <kursad.yildirim@outlook.com>
2024-06-14 01:28:56 +0200

    op generation ok, create pdf and api pending

--- main.go
+++ main.go
@@ -2,173 +2,192 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"math/rand"
-	"net/http"
-	"os"
 	"strconv"
 	"strings"
-	"time"
-
-	"github.com/jung-kurt/gofpdf"
 )
 
-// Define the operation struct
-type operation struct {
-	name string
-	sign string
+type config struct {
+	difficultyLevel int
+	apiPort         int
+	rangeMin        int
+	rangeMax        int
+	opCount         int
 }
 
-// Define operations as a slice of operation
+type operation struct {
+	name    string
+	sign    string
+	numType string
+}
+
 type operations []operation
 
-// Define the difficulty level constant (1 to 5, 5 being the hardest)
-const difficultyLevel = 1
-
-// Define number ranges based on the difficulty level
-var (
-	addMin, addMax   = getRange(1.0, 10.0)
-	subMin, subMax   = getRange(1.0, 10.0)
-	mulMin, mulMax   = getRange(1.0, 10.0)
-	divMin, divMax   = getRange(1, 10)
-	percMin, percMax = getRange(0, 100)
-	baseMin, baseMax = getRange(1, 10)
-	opCount          = 40
-)
-
-func getRange(baseMin, baseMax float64) (float64, float64) {
-	multiplier := float64(difficultyLevel)
-	return baseMin * multiplier, baseMax * multiplier
+type question struct {
+	opString       []string
+	opStringMasked []string
 }
+
+type questions []question
 
 func main() {
-	http.HandleFunc("/generate", generatePDFHandler)
-	fmt.Println("Server started at :3389")
-	http.ListenAndServe(":3389", nil)
+	var c config
+	var q questions
+	c.populate()
+	ops := operations{
+		{name: "addition", sign: "+", numType: "decimal"},
+		{name: "subtraction", sign: "-", numType: "decimal"},
+		{name: "multiplication", sign: "x", numType: "decimal"},
+		{name: "division", sign: "/", numType: "integer"},
+		{name: "percentage", sign: "%", numType: "integer"},
+	}
+	q.generateOps(c, ops)
+	q.alignOps()
+	for _, r := range q {
+		for _, x := range r.opString {
+			fmt.Printf("%v", x)
+		}
+		fmt.Println()
+	}
+	fmt.Println("===============================================================")
+	for _, r := range q {
+		for _, x := range r.opStringMasked {
+			fmt.Printf("%v", x)
+		}
+		fmt.Println()
+	}
+	// http.HandleFunc("/generate", generateOps())
+	// fmt.Printf("Server started at :%v", c.apiPort)
+	// http.ListenAndServe(":"+fmt.Sprintf("%s", c.apiPort), nil)
 }
 
+func (q *questions) generateOps(c config, ops operations) {
+	for i := 0; i < c.opCount; i++ {
+		*q = append(*q, generateSingleOp(c, ops[rand.Intn(len(ops))]))
+	}
+}
+
+func (q *questions) alignOps() {
+	col1 := (*q)[:len(*q)/2]
+	lenBefore, lenBeforeMasked := 0, 0
+	for _, r := range col1 {
+		if len(r.opString[0])+len(r.opString[2]) > lenBefore {
+			lenBefore = len(r.opString[0]) + len(r.opString[2])
+		}
+		if len(r.opStringMasked[0])+len(r.opStringMasked[2]) > lenBeforeMasked {
+			lenBeforeMasked = len(r.opStringMasked[0]) + len(r.opStringMasked[2])
+		}
+	}
+	for _, r := range col1 {
+		r.opString[0] = strings.Repeat(" ", lenBefore-len(r.opString[0])-len(r.opString[2])) + r.opString[0]
+		r.opStringMasked[0] = strings.Repeat(" ", lenBeforeMasked-len(r.opStringMasked[0])-len(r.opStringMasked[2])) + r.opStringMasked[0]
+	}
+	col2 := (*q)[len(*q)/2:]
+	lenBefore, lenBeforeMasked = 0, 0
+	for _, r := range col2 {
+		if len(r.opString[0])+len(r.opString[2]) > lenBefore {
+			lenBefore = len(r.opString[0]) + len(r.opString[2])
+		}
+		if len(r.opStringMasked[0])+len(r.opStringMasked[2]) > lenBeforeMasked {
+			lenBeforeMasked = len(r.opStringMasked[0]) + len(r.opStringMasked[2])
+		}
+	}
+	for _, r := range col2 {
+		r.opString[0] = strings.Repeat(" ", lenBefore-len(r.opString[0])-len(r.opString[2])) + r.opString[0]
+		r.opStringMasked[0] = strings.Repeat(" ", lenBeforeMasked-len(r.opStringMasked[0])-len(r.opStringMasked[2])) + r.opStringMasked[0]
+	}
+}
+
+func generateSingleOp(c config, op operation) question {
+	var numF1, numF2, resultF float64
+	var numI1, numI2, resultI int
+	var q question
+	switch op.name {
+	case "addition":
+		numF1 = getRandomDecimal(c)
+		numF2 = getRandomDecimal(c)
+		resultF = numF1 + numF2
+		q.opString = []string{formatNumber(numF1, 1), " " + op.sign + " ", formatNumber(numF2, 1), " = ", formatNumber(resultF, 2)}
+	case "subtraction":
+		numF1 = getRandomDecimal(c)
+		numF2 = getRandomDecimal(c)
+		resultF = numF1 - numF2
+		q.opString = []string{formatNumber(numF1, 1), " " + op.sign + " ", formatNumber(numF2, 1), " = ", formatNumber(resultF, 2)}
+	case "multiplication":
+		numI1 = getRandomInteger(c)
+		numI2 = getRandomInteger(c)
+		resultI = numI1 * numI2
+		q.opString = []string{fmt.Sprintf("%v", numI1), " " + op.sign + " ", fmt.Sprintf("%v", numI2), " = ", fmt.Sprintf("%v", resultI)}
+	case "division":
+		numI1 = getRandomInteger(c)
+		numI2 = getRandomInteger(c)
+		resultI = numI1 * numI2
+		q.opString = []string{fmt.Sprintf("%v", resultI), " " + op.sign + " ", fmt.Sprintf("%v", numI2), " = ", fmt.Sprintf("%v", numI1)}
+	case "percentage":
+		numI1 = getRandomInteger(c)
+		numI2 = getRandomInteger(c)
+		resultF = float64(numI1*numI2) / 100
+		q.opString = []string{fmt.Sprintf("%v", numI1), " " + op.sign + " ", fmt.Sprintf("%v", numI2), " = ", fmt.Sprintf("%v", resultF)}
+	}
+	q.opStringMasked = make([]string, len(q.opString))
+	copy(q.opStringMasked, q.opString)
+	maskIndex := rand.Intn(3)
+	if maskIndex == 0 {
+		q.opStringMasked[0] = "____"
+	} else if maskIndex == 1 {
+		q.opStringMasked[2] = "____"
+	} else {
+		q.opStringMasked[4] = "____"
+	}
+	return q
+}
+
+func getRandomInteger(c config) int {
+	n := rand.Intn(c.rangeMax * c.difficultyLevel)
+	if n == 0 {
+		return n + 1
+	} else {
+		return n
+	}
+}
+
+func getRandomDecimal(c config) float64 {
+	n := math.Round(rand.Float64()*float64(c.rangeMax*10*c.difficultyLevel)) / 10
+	if n == 0 {
+		return n + 0.1
+	} else {
+		return n
+	}
+}
+
+func formatNumber(num float64, p int) string {
+	return strconv.FormatFloat(num, 'f', p, 64)
+}
+
+func (c *config) populate() {
+	c.difficultyLevel = 5
+	c.apiPort = 8080
+	c.rangeMin = 0
+	c.rangeMax = 10
+	c.opCount = 40
+}
+
+/*
+
 func generatePDFHandler(w http.ResponseWriter, r *http.Request) {
-	// Seed the random number generator to get different results each time
-	rand.Seed(time.Now().UnixNano())
 
-	// Initialize the operations slice directly
-	ops := operations{
-		{name: "addition", sign: "+"},
-		{name: "subtraction", sign: "-"},
-		{name: "multiplication", sign: "x"},
-		{name: "division", sign: "/"},
-		{name: "percentage", sign: "%"},
-	}
-
-	// Prepare opCount random operations
-	var randomOps [][]string
-	var randomOpsWithAnswers [][]string
-
-	for i := 0; i < opCount; i++ {
-		randomIndex := rand.Intn(len(ops))
-		op := ops[randomIndex]
-
-		var num1, num2, result float64
-		var opString []string
-		var opWithAnswerString []string
-
-		switch op.name {
-		case "addition":
-			num1 = getRandomDecimal(addMin, addMax)
-			num2 = getRandomDecimal(addMin, addMax)
-			result = num1 + num2
-		case "subtraction":
-			num1 = getRandomDecimal(subMin, subMax)
-			num2 = getRandomDecimal(subMin, subMax)
-			result = num1 - num2
-		case "multiplication":
-			num1 = getRandomDecimal(mulMin, mulMax)
-			num2 = getRandomDecimal(mulMin, mulMax)
-			result = num1 * num2
-		case "division":
-			num1 = float64(getRandomInteger(int(divMin), int(divMax)))
-			num2 = float64(getRandomInteger(int(divMin), int(divMax)))
-			if num2 == 0 {
-				num2 = 1
-			}
-			result = num1 / num2
-		case "percentage":
-			num1 = float64(getRandomInteger(int(percMin), int(percMax))) // percentage
-			num2 = float64(getRandomInteger(int(baseMin), int(baseMax))) // base number
-			result = (num1 / 100) * num2
-		}
-
-		opString = []string{formatNumber(num1), op.sign, formatNumber(num2), "=", formatNumber(result)}
-		opWithAnswerString = make([]string, len(opString))
-		copy(opWithAnswerString, opString)
-
-		// Randomly mask one operand or the result
-		maskIndex := rand.Intn(3) // 0 for num1, 1 for num2, 2 for result
-		if maskIndex == 0 {
-			opString[0] = "_____"
-		} else if maskIndex == 1 {
-			opString[2] = "_____"
-		} else {
-			opString[4] = "_____"
-		}
-
-		randomOps = append(randomOps, opString)
-		randomOpsWithAnswers = append(randomOpsWithAnswers, opWithAnswerString)
-	}
-
-	// Align operations to ensure equal signs are vertically aligned
-	alignedQuestions := alignOperations(randomOps)
-	alignedAnswers := alignOperations(randomOpsWithAnswers)
-
-	// Generate a random 5-character index for the filename
 	index := generateRandomIndex()
 
-	// Generate PDF file with two pages and the index in the filename and title
 	filename := fmt.Sprintf("operations_%s.pdf", index)
 	title := fmt.Sprintf("Operations Practice - Level %d - %s", difficultyLevel, index)
 	createPDF(filename, alignedQuestions, alignedAnswers, title)
 
-	// Set headers to prompt the download with the suggested filename
 	w.Header().Set("Content-Disposition", fmt.Sprintf("attachment; filename=%s", filename))
 	w.Header().Set("Content-Type", "application/pdf")
 	http.ServeFile(w, r, filename)
 }
 
-func getRandomDecimal(min, max float64) float64 {
-	return min + rand.Float64()*(max-min)
-}
-
-func getRandomInteger(min, max int) int {
-	return min + rand.Intn(max-min+1)
-}
-
-func formatNumber(num float64) string {
-	return strconv.FormatFloat(num, 'f', 1, 64)
-}
-
-func alignOperations(ops [][]string) [][]string {
-	maxLen := [5]int{}
-
-	// Calculate maximum lengths for each part of the operations
-	for _, op := range ops {
-		for i, part := range op {
-			if len(part) > maxLen[i] {
-				maxLen[i] = len(part)
-			}
-		}
-	}
-
-	// Align operations
-	alignedOps := make([][]string, len(ops))
-	for i, op := range ops {
-		alignedOp := make([]string, len(op))
-		for j, part := range op {
-			alignedOp[j] = fmt.Sprintf("%-"+strconv.Itoa(maxLen[j])+"s", part)
-		}
-		alignedOps[i] = alignedOp
-	}
-
-	return alignedOps
-}
 
 func createPDF(filename string, questions [][]string, answers [][]string, title string) {
 	const (
@@ -184,14 +203,12 @@ func createPDF(filename string, questions [][]string, answers [][]string, title 
 	pdf := gofpdf.New("P", "mm", "A4", "")
 	pdf.SetFont("Arial", "", 12)
 
-	// Function to add a page of operations
 	addPage := func(ops [][]string, pageTitle string, header string) {
 		pdf.AddPage()
 		xLeft := margin
 		xRight := margin + columnWidth
 		y := margin
 
-		// Add header
 		pdf.SetFont("Arial", "B", 12)
 		pdf.SetXY(margin, y)
 		pdf.CellFormat(pageWidth-2*margin, 10, pageTitle, "0", 0, "C", false, 0, "")
@@ -201,7 +218,6 @@ func createPDF(filename string, questions [][]string, answers [][]string, title 
 		pdf.CellFormat(pageWidth-2*margin, 10, header, "0", 0, "L", false, 0, "")
 		y += headerHeight
 
-		// Add operations
 		for i := 0; i < rowsPerPage; i++ {
 			op1 := strings.Join(ops[i*2], " ")
 			op2 := ""
@@ -217,10 +233,8 @@ func createPDF(filename string, questions [][]string, answers [][]string, title 
 		}
 	}
 
-	// Add questions page with full header
 	addPage(questions, title, "Name: _______________________________          Time: _____          Score: _____ / 40")
 
-	// Add answers page with a simpler header
 	addPage(answers, "----- Answers -----", "")
 
 	err := pdf.OutputFileAndClose(filename)
@@ -238,3 +252,4 @@ func generateRandomIndex() string {
 	}
 	return string(result)
 }
+*/
